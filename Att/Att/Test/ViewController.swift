10d052db5bc5cbec80cf53707f0b5dbc8a664f95
jeonghyeon Hwang <hijihilin@gmail.com>
2023-07-09 19:13:14 +0900

    [BUILD] #1 프로젝트 기본 설정

---
+++ Att/Att/Test/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Att
+//
+//  Created by 황정현 on 2023/07/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
