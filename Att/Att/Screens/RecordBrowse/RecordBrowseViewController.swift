ee86d40469386dfe62f48cca4e1543051fc49eeb
jeonghyeon Hwang <hijihilin@gmail.com>
2023-09-11 21:30:52 +0900

    [STYLE] #10 폰트 및 더미 데이터 수정

--- Att/Att/Screens/RecordBrowse/RecordBrowseViewController.swift
+++ Att/Att/Screens/RecordBrowse/RecordBrowseViewController.swift
@@ -25,7 +25,7 @@ final class RecordBrowseViewController: UIViewController {
     private lazy var editButton: UIButton = {
         let button = UIButton()
         button.setTitle("수정하기", for: .normal)
-        button.titleLabel?.font = .semibold15
+        button.titleLabel?.font = .caption
         button.setTitleColor(.black, for: .normal)
         return button
     }()
@@ -40,7 +40,7 @@ final class RecordBrowseViewController: UIViewController {
     
     private lazy var publicationTimeLabel: UILabel = {
         let label = UILabel()
-        label.font = .caption4
+        label.font = .caption2
         label.textAlignment = .right
         label.textColor = .black
         return label
@@ -67,7 +67,7 @@ final class RecordBrowseViewController: UIViewController {
     }()
     
     private lazy var diaryView: DiaryView = {
-        let view = DiaryView(title: "Diary")
+        let view = DiaryView(title: "오늘의 일기")
         return view
     }()
     
@@ -208,7 +208,7 @@ final class RecordBrowseViewController: UIViewController {
     // MARK: ViewModel Stuff - Optional
     private func bind() {
         dateLabel.text = Date().date()
-        publicationTimeLabel.text = "발행시간 2023. 5. 25. KST 23:50"
+        publicationTimeLabel.text = "발행시간 2023. 9. 11. KST 23:50"
         todaysMoodView.setUpColor(color: .cherry)
         nowPlayingView.setUpComponent(title: "누군가 - 부르는 노래", image: UIImage())
         fromYesterdayView.setUpComponent(text: "열심히 무언가를 하다")
--- Att/Att/Util/Extensions/UIFont+.swift
+++ Att/Att/Util/Extensions/UIFont+.swift
@@ -21,10 +21,15 @@ extension UIFont {
         }
     }
     
+    // TODO: Design System 개편 시 수정
+    static let largeTitle = UIFont(weight: .bold, size: 40)
+    static let smallTitle = UIFont(weight: .bold, size: 24)
+    
     static let title0 = UIFont(weight: .bold, size: 36)
     static let title1 = UIFont(weight: .bold, size: 32)
     static let title2 = UIFont(weight: .bold, size: 28)
     static let title3 = UIFont(weight: .bold, size: 22)
+    static let title4 = UIFont(weight: .bold, size: 20)
     
     static let subtitle1 = UIFont(weight: .regular, size: 28)
     static let subtitle2 = UIFont(weight: .regular, size: 24)
@@ -32,4 +37,7 @@ extension UIFont {
     static let caption1 = UIFont(weight: .regular, size: 16)
     static let caption2 = UIFont(weight: .regular, size: 14)
     static let caption3 = UIFont(weight: .regular, size: 12)
+    
+    // TODO: Design System 개편 시 수정
+    static let caption = UIFont(weight: .regular, size: 15)
 }
